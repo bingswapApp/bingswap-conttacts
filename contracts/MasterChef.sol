3563c1249aaecb9a9c79651b97747de70186b155
bingswapApp <80484544+bingswapApp@users.noreply.github.com>
2021-03-12 03:55:36 +0800

    Update MasterChef.sol

--- contracts/MasterChef.sol
+++ contracts/MasterChef.sol
@@ -9,12 +9,12 @@ import "./libs/ISailReferral.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
-import "./SailToken.sol";
+import "./BingToken.sol";
 
-// MasterChef is the master of Sail. He can make Sail and he is a fair guy.
+// MasterChef is the master of Sail. He can make Bing and he is a fair guy.
 //
 // Note that it's ownable and the owner wields tremendous power. The ownership
-// will be transferred to a governance smart contract once SAIL is sufficiently
+// will be transferred to a governance smart contract once BING is sufficiently
 // distributed and the community can show to govern itself.
 //
 // Have fun reading it. Hopefully it's bug-free. God bless.
@@ -27,13 +27,13 @@ contract MasterChef is Ownable, ReentrancyGuard {
         uint256 amount;         // How many LP tokens the user has provided.
         uint256 rewardDebt;     // Reward debt. See explanation below.
         //
-        // We do some fancy math here. Basically, any point in time, the amount of SAILs
+        // We do some fancy math here. Basically, any point in time, the amount of BINGs
         // entitled to a user but is pending to be distributed is:
         //
-        //   pending reward = (user.amount * pool.accSailPerShare) - user.rewardDebt
+        //   pending reward = (user.amount * pool.accBingPerShare) - user.rewardDebt
         //
         // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
-        //   1. The pool's `accSailPerShare` (and `lastRewardBlock`) gets updated.
+        //   1. The pool's `accBingPerShare` (and `lastRewardBlock`) gets updated.
         //   2. User receives the pending reward sent to his/her address.
         //   3. User's `amount` gets updated.
         //   4. User's `rewardDebt` gets updated.
@@ -42,26 +42,26 @@ contract MasterChef is Ownable, ReentrancyGuard {
     // Info of each pool.
     struct PoolInfo {
         IBEP20 lpToken;           // Address of LP token contract.
-        uint256 allocPoint;       // How many allocation points assigned to this pool. SAILs to distribute per block.
-        uint256 lastRewardBlock;  // Last block number that SAILs distribution occurs.
-        uint256 accSailPerShare;   // Accumulated SAILs per share, times 1e12. See below.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. BINGs to distribute per block.
+        uint256 lastRewardBlock;  // Last block number that BINGs distribution occurs.
+        uint256 accSailPerShare;   // Accumulated BINGs per share, times 1e12. See below.
         uint16 depositFeeBP;      // Deposit fee in basis points
     }
 
-    // The SAIL TOKEN!
-    SailToken public sail;
+    // The BING TOKEN!
+    BingToken public bing;
     // Dev address.
     address public devAddress;
     // Deposit Fee address
     address public feeAddress;
-    // SAIL tokens created per block.
-    uint256 public sailPerBlock;
+    // BING tokens created per block.
+    uint256 public bingPerBlock;
     // Bonus muliplier for early sail makers.
     uint256 public constant BONUS_MULTIPLIER = 1;
 
-    // Initial emission rate: 1 SAIL per block.
+    // Initial emission rate: 1 BING per block.
     uint256 public constant INITIAL_EMISSION_RATE = 1 ether;
-    // Minimum emission rate: 0.1 SAIL per block.
+    // Minimum emission rate: 0.1 BING per block.
     uint256 public constant MINIMUM_EMISSION_RATE = 100 finney;
     // Reduce emission every 9,600 blocks ~ 8 hours.
     uint256 public constant EMISSION_REDUCTION_PERIOD_BLOCKS = 9600;
@@ -76,11 +76,11 @@ contract MasterChef is Ownable, ReentrancyGuard {
     mapping(uint256 => mapping(address => UserInfo)) public userInfo;
     // Total allocation points. Must be the sum of all allocation points in all pools.
     uint256 public totalAllocPoint = 0;
-    // The block number when SAIL mining starts.
+    // The block number when BING mining starts.
     uint256 public startBlock;
 
-    // Sail referral contract address.
-    ISailReferral public sailReferral;
+    // Bing referral contract address.
+    IBingReferral public bingReferral;
     // Referral commission rate in basis points.
     uint16 public referralCommissionRate = 200;
     // Max referral commission rate: 20%.
@@ -93,15 +93,15 @@ contract MasterChef is Ownable, ReentrancyGuard {
     event ReferralCommissionPaid(address indexed user, address indexed referrer, uint256 commissionAmount);
 
     constructor(
-        SailToken _sail,
+        BingToken _bing,
         uint256 _startBlock
     ) public {
-        sail = _sail;
+        bing = _bing;
         startBlock = _startBlock;
 
         devAddress = msg.sender;
         feeAddress = msg.sender;
-        sailPerBlock = INITIAL_EMISSION_RATE;
+        bingPerBlock = INITIAL_EMISSION_RATE;
     }
 
     function poolLength() external view returns (uint256) {
@@ -121,12 +121,12 @@ contract MasterChef is Ownable, ReentrancyGuard {
             lpToken: _lpToken,
             allocPoint: _allocPoint,
             lastRewardBlock: lastRewardBlock,
-            accSailPerShare: 0,
+            accBingPerShare: 0,
             depositFeeBP: _depositFeeBP
         }));
     }
 
-    // Update the given pool's SAIL allocation point and deposit fee. Can only be called by the owner.
+    // Update the given pool's BING allocation point and deposit fee. Can only be called by the owner.
     function set(uint256 _pid, uint256 _allocPoint, uint16 _depositFeeBP, bool _withUpdate) public onlyOwner {
         require(_depositFeeBP <= 10000, "set: invalid deposit fee basis points");
         if (_withUpdate) {
@@ -183,7 +183,7 @@ contract MasterChef is Ownable, ReentrancyGuard {
         pool.lastRewardBlock = block.number;
     }
 
-    // Deposit LP tokens to MasterChef for SAIL allocation.
+    // Deposit LP tokens to MasterChef for BING allocation.
     function deposit(uint256 _pid, uint256 _amount, address _referrer) public nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
@@ -192,9 +192,9 @@ contract MasterChef is Ownable, ReentrancyGuard {
             sailReferral.recordReferral(msg.sender, _referrer);
         }
         if (user.amount > 0) {
-            uint256 pending = user.amount.mul(pool.accSailPerShare).div(1e12).sub(user.rewardDebt);
+            uint256 pending = user.amount.mul(pool.accBingPerShare).div(1e12).sub(user.rewardDebt);
             if (pending > 0) {
-                safeSailTransfer(msg.sender, pending);
+                safeBingTransfer(msg.sender, pending);
                 payReferralCommission(msg.sender, pending);
             }
         }
@@ -212,7 +212,7 @@ contract MasterChef is Ownable, ReentrancyGuard {
                 user.amount = user.amount.add(_amount);
             }
         }
-        user.rewardDebt = user.amount.mul(pool.accSailPerShare).div(1e12);
+        user.rewardDebt = user.amount.mul(pool.accBingPerShare).div(1e12);
         emit Deposit(msg.sender, _pid, _amount);
     }
 
@@ -222,16 +222,16 @@ contract MasterChef is Ownable, ReentrancyGuard {
         UserInfo storage user = userInfo[_pid][msg.sender];
         require(user.amount >= _amount, "withdraw: not good");
         updatePool(_pid);
-        uint256 pending = user.amount.mul(pool.accSailPerShare).div(1e12).sub(user.rewardDebt);
+        uint256 pending = user.amount.mul(pool.accBingPerShare).div(1e12).sub(user.rewardDebt);
         if (pending > 0) {
-            safeSailTransfer(msg.sender, pending);
+            safeBingTransfer(msg.sender, pending);
             payReferralCommission(msg.sender, pending);
         }
         if (_amount > 0) {
             user.amount = user.amount.sub(_amount);
             pool.lpToken.safeTransfer(address(msg.sender), _amount);
         }
-        user.rewardDebt = user.amount.mul(pool.accSailPerShare).div(1e12);
+        user.rewardDebt = user.amount.mul(pool.accBingPerShare).div(1e12);
         emit Withdraw(msg.sender, _pid, _amount);
     }
 
@@ -246,16 +246,16 @@ contract MasterChef is Ownable, ReentrancyGuard {
         emit EmergencyWithdraw(msg.sender, _pid, amount);
     }
 
-    // Safe sail transfer function, just in case if rounding error causes pool to not have enough SAILs.
+    // Safe Bing transfer function, just in case if rounding error causes pool to not have enough BINGs.
     function safeSailTransfer(address _to, uint256 _amount) internal {
-        uint256 sailBal = sail.balanceOf(address(this));
+        uint256 sailBal = bing.balanceOf(address(this));
         bool transferSuccess = false;
         if (_amount > sailBal) {
-            transferSuccess = sail.transfer(_to, sailBal);
+            transferSuccess = bing.transfer(_to, bingBal);
         } else {
-            transferSuccess = sail.transfer(_to, _amount);
+            transferSuccess = bing.transfer(_to, _amount);
         }
-        require(transferSuccess, "safeSailTransfer: Transfer failed");
+        require(transferSuccess, "safeBingTransfer: Transfer failed");
     }
 
     // Update dev address by the previous dev.
@@ -279,26 +279,27 @@ contract MasterChef is Ownable, ReentrancyGuard {
             return;
         }
 
-        uint256 newEmissionRate = sailPerBlock;
+        uint256 newEmissionRate = bingPerBlock;
         for (uint256 index = lastReductionPeriodIndex; index < currentIndex; ++index) {
             newEmissionRate = newEmissionRate.mul(1e4 - EMISSION_REDUCTION_RATE_PER_PERIOD).div(1e4);
         }
 
         newEmissionRate = newEmissionRate < MINIMUM_EMISSION_RATE ? MINIMUM_EMISSION_RATE : newEmissionRate;
-        if (newEmissionRate >= sailPerBlock) {
+        if (newEmissionRate >= bingPerBlock) {
             return;
         }
 
         massUpdatePools();
         lastReductionPeriodIndex = currentIndex;
-        uint256 previousEmissionRate = sailPerBlock;
+        uint256 previousEmissionRate = bingPerBlock;
         sailPerBlock = newEmissionRate;
         emit EmissionRateUpdated(msg.sender, previousEmissionRate, newEmissionRate);
     }
 
-    // Update the sail referral contract address by the owner
-    function setSailReferral(ISailReferral _sailReferral) public onlyOwner {
-        sailReferral = _sailReferral;
+    // Update the bing referral contract address by the owner
+    function setBingReferral(IBingReferral _bingReferral) public onlyOwner {
+
+        bingReferral = _bingReferral;
     }
 
     // Update referral commission rate by the owner
@@ -309,12 +310,12 @@ contract MasterChef is Ownable, ReentrancyGuard {
 
     // Pay referral commission to the referrer who referred this user.
     function payReferralCommission(address _user, uint256 _pending) internal {
-        if (address(sailReferral) != address(0) && referralCommissionRate > 0) {
-            address referrer = sailReferral.getReferrer(_user);
+        if (address(bingReferral) != address(0) && referralCommissionRate > 0) {
+            address referrer = bingReferral.getReferrer(_user);
             uint256 commissionAmount = _pending.mul(referralCommissionRate).div(10000);
 
             if (referrer != address(0) && commissionAmount > 0) {
-                sail.mint(referrer, commissionAmount);
+                bing.mint(referrer, commissionAmount);
                 emit ReferralCommissionPaid(_user, referrer, commissionAmount);
             }
         }
